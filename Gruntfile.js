7a9e772c12611f63c510b95729f232fe9801938f
Justin DuJardin <justin@dujardinconsulting.com>
2014-08-09 13:33:50 -0700

    feat(grunt): add artifacts task for generating tag release outputs
    This removes the need to have the lib/ directory checked in while still versioning the files in lib/ for release tags.  This means that bower and people who check out release tags will have prebuilt assets, but developers don't need to worry about not accidentally committing them.

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,12 +77,7 @@ module.exports = function(grunt) {
             commitFiles: [
                'package.json',
                'bower.json',
-               'CHANGELOG.md',
-               'lib/pow-core.d.ts',
-               'lib/pow-core.js',
-               'lib/pow-core.js.map',
-               'lib/pow-core.min.js',
-               'lib/pow-core.min.map'
+               'CHANGELOG.md'
             ],
             createTag: true,
             tagName: 'v%VERSION%',
@@ -90,6 +85,17 @@ module.exports = function(grunt) {
             push: true,
             pushTo: 'origin',
             gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+         }
+      },
+      artifacts: {
+         options:{
+            files: [
+               'lib/pow-core.d.ts',
+               'lib/pow-core.js',
+               'lib/pow-core.js.map',
+               'lib/pow-core.min.js',
+               'lib/pow-core.min.map'
+            ]
          }
       },
       changelog: {},
@@ -136,9 +142,58 @@ module.exports = function(grunt) {
          'npm-contributors',
          'typescript:source',
          'uglify',
-         "bump:" + type + ":bump-only",
+         'bump:' + type + ':bump-only',
          'changelog',
-         'bump-commit'
+         'artifacts:add',
+         'bump-commit',
+         'artifacts:remove'
       ]);
    });
+
+
+   var exec = require('child_process').exec;
+   grunt.registerTask('artifacts', 'temporarily version output libs for release tags', function(type) {
+      var opts = this.options({
+         files: [],
+         commitAdd:"chore: add artifacts for release",
+         commitRemove:"chore: remove release artifacts"
+      });
+      var done = this.async();
+      console.log(opts.files);
+      if(type === 'add') {
+         exec('git add ' + opts.files.join(' '), function (err, stdout, stderr) {
+            if (err) {
+               grunt.fatal('Cannot add the release artifacts:\n  ' + stderr);
+            }
+            var commitMessage = opts.commitAdd;
+            exec('git commit ' + opts.files.join(' ') + ' -m "' + commitMessage + '"', function (err, stdout, stderr) {
+               if (err) {
+                  grunt.fatal('Cannot create the commit:\n  ' + stderr);
+               }
+               grunt.log.ok('Committed as "' + commitMessage + '"');
+               done();
+            });
+         });
+      }
+      else if(type === 'remove'){
+         exec('git rm -f ' + opts.files.join(' ') + ' --cached', function(err, stdout, stderr) {
+            if (err) {
+               grunt.fatal('Cannot remove the release artifacts:\n  ' + stderr);
+            }
+            var commitMessage = opts.commitRemove;
+            exec('git commit -m "' + commitMessage + '"', function(err, stdout, stderr) {
+               if (err) {
+                  grunt.fatal('Cannot create the commit:\n  ' + stderr);
+               }
+               grunt.log.ok('Committed as "' + commitMessage + '"');
+               done();
+            });
+         });
+      }
+      else {
+         grunt.fatal('Invalid type to artifacts');
+      }
+   });
+
+
 };
