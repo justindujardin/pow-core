5d91007259a2c96d05a9f45dd561d53257c98912
Justin DuJardin <justin@dujardinconsulting.com>
2014-08-08 18:28:28 -0700

    test(Events): better coverage of events

--- test/eventsTests.ts
+++ test/eventsTests.ts
@@ -1,11 +1,6 @@
 ///<reference path="../types/jasmine.d.ts"/>
 ///<reference path="../lib/pow-core.d.ts"/>
 
-//on(name: any, callback?: Function, context?: any): IEvents;
-//off(name?: string, callback?: Function, context?: any): IEvents;
-//once(events: string, callback: Function, context?: any): IEvents;
-//trigger(name: string, ...args: any[]): IEvents;
-
 describe("pow2.Events",()=>{
    it("should be defined",()=>{
       expect(pow2.Events).toBeDefined();
@@ -24,6 +19,8 @@ describe("pow2.Events",()=>{
 
    it("should add observers with on",()=>{
       var ev:string = 'event';
+      obj.trigger(ev);
+      expect(count).toBe(0);
       obj.on(ev,cb);
       obj.trigger(ev);
       expect(count).toBe(1);
@@ -46,5 +43,39 @@ describe("pow2.Events",()=>{
       obj.trigger(ev);
       obj.trigger(ev);
       expect(count).toBe(1);
+   });
+
+   it("should support trigger with any number of arguments",()=>{
+      var ev:string = 'event';
+      var expectArgs:number = -1;
+      var cb:any = () => {
+         expect(expectArgs).toBeGreaterThan(-1);
+         expect(arguments.length).toBe(expectArgs);
+      };
+      obj.on(ev,cb);
+      var i:number = 0;
+      for(; i < 10; i++){
+         expectArgs = i;
+         var values = new Array(i);
+         for(var j:number = 0; j < values.length; j++){
+            values[j] = j;
+         }
+         values.unshift(ev);
+         obj.trigger.apply(obj,values);
+      }
+   });
+
+   it("should support multiple event names",()=>{
+      var ev:string = 'event';
+      obj.on(ev,cb);
+      obj.trigger('event event');
+      expect(count).toBe(2);
+   });
+
+   it("should support object listener syntax",()=>{
+      obj.on({
+         'event': cb
+      });
+      obj.trigger('event');
    });
 });
