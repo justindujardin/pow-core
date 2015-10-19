3d38953056d645b3bf68a2ed51a6db631f8c27c9
Justin DuJardin <justin@dujardinconsulting.com>
2015-10-18 18:26:11 -0700

    feat(Sauce): expand browser coverage considerably

--- karma.sauce.conf.js
+++ karma.sauce.conf.js
@@ -3,35 +3,101 @@ module.exports = function (config) {
   require('./karma.conf')(config);
 
   var customLaunchers = {
-    'SL_Chrome': {
+    'SL_CHROME': {
       base: 'SauceLabs',
       browserName: 'chrome',
-      platform: 'Windows 8.1',
-      version: '42'
+      version: '45'
     },
-    'SL_ChromeBeta': {
+    'SL_CHROMEBETA': {
       base: 'SauceLabs',
       browserName: 'chrome',
-      platform: 'OS X 10.9',
       version: 'beta'
     },
-    'SL_Firefox': {
+    'SL_CHROMEDEV': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      version: 'dev'
+    },
+    'SL_FIREFOX': {
       base: 'SauceLabs',
       browserName: 'firefox',
-      platform: 'OS X 10.9',
-      version: '37'
+      version: '40'
     },
-    'SL_Safari7': {
+    'SL_FIREFOXBETA': {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: 'beta'
+    },
+    'SL_FIREFOXDEV': {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: 'dev'
+    },
+    'SL_SAFARI7': {
       base: 'SauceLabs',
       browserName: 'safari',
       platform: 'OS X 10.9',
       version: '7'
     },
-    'SL_IE_11': {
+    'SL_SAFARI8': {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      platform: 'OS X 10.10',
+      version: '8'
+    },
+    'SL_SAFARI8.1': {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      platform: 'OS X 10.11',
+      version: '8.1'
+    },
+    'SL_IOS8': {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.10',
+      version: '8.4'
+    },
+    'SL_IOS9': {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.10',
+      version: '9.0'
+    },
+    'SL_IE9': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 2008',
+      version: '9'
+    },
+    'SL_IE10': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 2012',
+      version: '10'
+    },
+    'SL_IE11': {
       base: 'SauceLabs',
       browserName: 'internet explorer',
       platform: 'Windows 8.1',
       version: '11'
+    },
+    'SL_EDGE': {
+      base: 'SauceLabs',
+      browserName: 'microsoftedge',
+      platform: 'Windows 10',
+      version: '20.10240'
+    },
+    'SL_ANDROID4.4': {
+      base: 'SauceLabs',
+      browserName: 'android',
+      platform: 'Linux',
+      version: '4.4'
+    },
+    'SL_ANDROID5.1': {
+      base: 'SauceLabs',
+      browserName: 'android',
+      platform: 'Linux',
+      version: '5.1'
     }
   };
 
@@ -45,7 +111,7 @@ module.exports = function (config) {
       recordVideo: false,
       recordScreenshots: false,
       options: {
-        'selenium-version': '2.45.0',
+        'selenium-version': '2.47.1',
         'command-timeout': 600,
         'idle-timeout': 600,
         'max-duration': 5400
